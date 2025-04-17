a4e66989ca55d20a03097344224884a39acbc8ae
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-04-17 08:33:59 -0400

    add times

--- mdopt/main.py
+++ mdopt/main.py
@@ -6,6 +6,7 @@ from rdkit.Chem import rdMolAlign
 from rdkit.Geometry import Point3D
 from ase.calculators.emt import EMT
 from mace.calculators import mace_off
+import time
 from tqdm import tqdm 
 import numpy as np
 import os
@@ -82,7 +83,7 @@ def optimize_molecules(
     calc = mace_off(model="large", device='cuda')
 
     # <Converged?> <Initial RMSE from DFT> <Final RMSE from DFT>
-    out_data = np.zeros((len(dft_molecules.keys()), 3))
+    out_data = np.zeros((len(dft_molecules.keys()), 4))
 
     # subset = list(dft_molecules.keys())[:20]
 
@@ -91,12 +92,14 @@ def optimize_molecules(
             rdkit_molecule = generate_molecule_from_smiles(smiles_str)
             ase_atoms, my_mol = rdkit_mol_to_ase_atoms(rdkit_molecule, calc)
             initial_rmse = my_mol.rmse(dft_molecules[smiles_str])
+            start = time.time()
             converged, optmized_mol = ase_optimize_molecule(ase_atoms, outpath, smiles_str, tol, maxsteps)
+            end = time.time()
             final_rmse = MyMolecule.from_ase(optmized_mol).rmse(dft_molecules[smiles_str])
-            out_data[i,:] = [converged, initial_rmse, final_rmse]
+            out_data[i,:] = [converged, initial_rmse, final_rmse, end - start]
         except:
             print(f"Failed for molecule: {smiles_str}")
-            out_data[i,:] = [0.0, np.nan, np.nan]
+            out_data[i,:] = [0.0, np.nan, np.nan, np.nan]
             # raise
 
     print(f"{int(len(out_data[:,0]) - sum(out_data[:,0]))} did NOT converge")
